d10ff0ba85b0965dcaa772065c8956bbe5c5bdc4
dimaspaiva <dimasalpaiva@gmail.com>
2020-08-31 17:16:54 -0300

    first task, fetch without Relay

--- workshop/01-fetchGraphql/src/App.tsx
+++ workshop/01-fetchGraphql/src/App.tsx
@@ -1,22 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Flex, Text } from 'rebass';
 import { Card, Content, Button } from '@workshop/ui';
 
-const App = () => {
-  const posts = [];
-  const error = false;
+import config from './config';
 
-  /**
-   * @TODO
-   * Fetch posts to be rendered in this component
-   */
+interface Post {
+  content: string;
+  id: string;
+}
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(false);
+  const [nextPage, setNextPage] = useState(false);
+
+  const handleFetch = async (query: string) => {
+    setError(false);
+
+    const response = await fetch(config.GRAPHQL_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+      }),
+    });
+
+    return response.json();
+  };
+
+  const handlePosts = data => {
+    const posts = data.posts.edges.map(({ node }: { node: Post }) => ({ id: node.id, content: node.content }));
+    const { hasNextPage, endCursor } = data.posts.pageInfo;
+    if (hasNextPage) {
+      setNextPage(endCursor);
+    }
+    setNextPage(false);
+    setPosts(posts);
+  };
+
+  const handleRequestPosts = (pagination?: string) => {
+    const queryVars =
+      pagination === 'after'
+        ? {
+            declare: '($nexPage: String)',
+            value: `after: ${nextPage}`,
+          }
+        : {
+            declare: '($prevPage: String)',
+            value: `before: ${nextPage}`,
+          };
+
+    const query = `
+    query getPosts ${nextPage ? queryVars.declare : ''} {
+      posts(first: 10 ${nextPage ? queryVars.value : ''}) {
+        edges {
+          node {
+            id
+            content
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+    }
+  `;
+
+    handleFetch(query)
+      .then(({ data }) => {
+        handlePosts(data);
+      })
+      .catch(() => setError(true));
+  };
+
+  useEffect(() => {
+    handleRequestPosts();
+  }, []);
 
   if (error) {
     return (
       <Content>
         <Text>Error: {error}</Text>
-        <Button mt='10px'>retry</Button>
+        <Button mt='10px' onClick={() => handleRequestPosts()}>
+          retry
+        </Button>
       </Content>
     );
   }
@@ -26,7 +98,7 @@ const App = () => {
       <Flex flexDirection='column'>
         <Text>Posts</Text>
         <Flex flexDirection='column'>
-          {posts.map(post => (
+          {posts.map((post: Post) => (
             <Card key={post.id} mt='10px' flexDirection='column' p='10px'>
               <Text>id: {post.id}</Text>
               <Text>content: {post.content}</Text>
@@ -34,8 +106,8 @@ const App = () => {
           ))}
         </Flex>
       </Flex>
-      <Button>Prev</Button>
-      <Button>Next</Button>
+      {nextPage && <Button onClick={() => handleRequestPosts('before')}>Prev</Button>}
+      {nextPage && <Button onClick={() => handleRequestPosts('after')}>Next</Button>}
     </Content>
   );
 };
