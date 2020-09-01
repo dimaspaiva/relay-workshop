a76a1584ade7109716b209d125ac946fe3f0c77a
dimaspaiva <dimasalpaiva@gmail.com>
2020-09-01 18:01:49 -0300

    feat: create post component and list author

--- workshop/03-useFragment/src/App.tsx
+++ workshop/03-useFragment/src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 
 import { Flex, Text } from 'rebass';
-import { Content, Card } from '@workshop/ui';
+import { Content } from '@workshop/ui';
 
 import { useLazyLoadQuery, graphql } from 'react-relay/hooks';
+
+import Post from './Post';
 
 import { AppQuery } from './__generated__/AppQuery.graphql';
 
 const App = () => {
-  const response = useLazyLoadQuery<AppQuery>(
+  const { posts } = useLazyLoadQuery<AppQuery>(
     graphql`
       query AppQuery {
         posts(first: 10) {
           edges {
             node {
               id
-              content
+              ...Post_app
             }
           }
         }
@@ -27,18 +29,13 @@ const App = () => {
     },
   );
 
-  const { posts } = response;
-
   return (
     <Content>
       <Flex flexDirection='column'>
         <Text>Posts</Text>
         <Flex flexDirection='column'>
           {posts.edges.map(({ node }) => (
-            <Card mt='10px' flexDirection='column' p='10px' key={node}>
-              <Text>id: {node.id}</Text>
-              <Text>content: {node.content}</Text>
-            </Card>
+            <Post key={node.id} app={node} />
           ))}
         </Flex>
       </Flex>
--- workshop/03-useFragment/src/Post.tsx
+++ workshop/03-useFragment/src/Post.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Text } from 'rebass';
 import { Card } from '@workshop/ui';
+import { useFragment, graphql } from 'react-relay/hooks';
+
+import { Post_app$key } from './__generated__/Post_app.graphql';
 
 /**
  * TODO
  * useFragment to let Post declare its data requirement
  */
 
-// eslint-disable-next-line
-const Post = props => {
+interface Props {
+  app: Post_app$key;
+}
+
+const Post = ({ app }: Props) => {
+  const query = graphql`
+    fragment Post_app on Post {
+      id
+      content
+      author {
+        name
+      }
+    }
+  `;
+
+  const post = useFragment(query, app);
+
   return (
     <Card mt='10px' flexDirection='column' p='10px'>
       <Text>id: {post.id}</Text>
-      <Text>content: {post.content}</Text>
+      <Text>Content: {post.content}</Text>
+      <Text>Author: {post.author?.name}</Text>
     </Card>
   );
 };
