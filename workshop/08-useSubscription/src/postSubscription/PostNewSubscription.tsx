2aa0d375209c486801374ccc5b2f1ded03b15f09
dimaspaiva <dimasalpaiva@gmail.com>
2020-09-03 16:47:27 -0300

    feat: add newPost subscription and snack alerts

--- workshop/08-useSubscription/src/postSubscription/PostNewSubscription.tsx
+++ workshop/08-useSubscription/src/postSubscription/PostNewSubscription.tsx
@@ -1,17 +1,28 @@
 // eslint-disable-next-line
 import { graphql } from 'react-relay';
 // eslint-disable-next-line
-import { ConnectionHandler, RecordSourceSelectorProxy, ROOT_ID } from 'relay-runtime';
+import { RecordSourceSelectorProxy, ROOT_ID, ConnectionHandler, RecordProxy } from 'relay-runtime';
 
 // eslint-disable-next-line
 import { connectionUpdater } from '@workshop/relay';
 
-/**
- * TODO
- * add PostNew subscription here
- */
-
-export const PostNew = '';
+export const PostNew = graphql`
+  subscription PostNewSubscription($input: PostNewInput!) {
+    PostNew(input: $input) {
+      post {
+        id
+        content
+        author {
+          id
+          name
+        }
+        meHasLiked
+        likesCount
+        ...PostComments_post
+      }
+    }
+  }
+`;
 
 /**
  * TODO
@@ -19,4 +30,48 @@ export const PostNew = '';
  * avoid duplication of post
  */
 // eslint-disable-next-line
-export const updater = (store: RecordSourceSelectorProxy) => {};
+const hasPostStore = (node: RecordProxy, store: RecordSourceSelectorProxy) => {
+  const id = node.getDataID();
+
+  if (!id) {
+    return;
+  }
+
+  return store.get(id);
+};
+
+const getConnection = (store: RecordSourceSelectorProxy) => {
+  const parentProxy = store.getRoot();
+
+  if (!parentProxy) {
+    return;
+  }
+
+  return ConnectionHandler.getConnection(parentProxy, 'Feed_posts'); // QueryName_edgeName
+};
+
+export const updater = (store: RecordSourceSelectorProxy) => {
+  const postNode = store.getRootField('PostNew')?.getLinkedRecord('post');
+
+  if (!postNode) {
+    return;
+  }
+
+  if (!hasPostStore(postNode, store)) {
+    const connection = getConnection(store);
+
+    if (!connection) {
+      return;
+    }
+
+    const postEdge = ConnectionHandler.createEdge(store, connection, postNode, 'PostEdge');
+
+    connectionUpdater({
+      store,
+      parentId: ROOT_ID,
+      connectionName: 'Feed_posts',
+      edge: postEdge,
+      before: true,
+    });
+  }
+};
