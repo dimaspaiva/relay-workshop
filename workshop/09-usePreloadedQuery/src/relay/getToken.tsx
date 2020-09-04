d92226cb418571cc7b20baf7171f40db86768dfd
dimaspaiva <dimasalpaiva@gmail.com>
2020-09-04 08:26:16 -0300

    upd: add token

--- workshop/09-usePreloadedQuery/src/relay/getToken.tsx
+++ workshop/09-usePreloadedQuery/src/relay/getToken.tsx
@@ -1,4 +1,4 @@
 // read from localstorage or cookie
 export const getToken = () => {
-  return 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOWMyY2I0NTI5NjJiNmNjZTA3OGNmMCIsImlhdCI6MTU4NzI5MzM2NH0.ieeCANyYeNMwbSF2MH-lYFsLNO_ZWqWsP8H3M-KF7mA';
+  return 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMGQxN2JmOTY4Yzk4Y2E4MGNjMzUzYSIsImlhdCI6MTU5OTIxNzk4MX0.CyKZiouRBQT6iR6kYJ4qsWxAOOPJAVIUUUqj4R_5Bj4';
 };
