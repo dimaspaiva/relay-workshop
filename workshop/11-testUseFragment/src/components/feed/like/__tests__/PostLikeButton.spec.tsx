e706f9e9b4d39ffdf621cc55a809c0add2468162
dimaspaiva <dimasalpaiva@gmail.com>
2020-09-08 16:58:16 -0300

    test: create tests, to watch if likes count exists

--- workshop/11-testUseFragment/src/components/feed/like/__tests__/PostLikeButton.spec.tsx
+++ workshop/11-testUseFragment/src/components/feed/like/__tests__/PostLikeButton.spec.tsx
@@ -13,18 +13,23 @@ import PostLikeButton from '../PostLikeButton';
 
 import { withProviders } from '../../../../../test/withProviders';
 
+import { PostLikeButtonSpecQuery } from './__generated__/PostLikeButtonSpecQuery.graphql';
+
 // eslint-disable-next-line
 export const getRoot = ({ preloadedQuery }) => {
   const UseQueryWrapper = () => {
-    /**
-     * TODO
-     * add usePreloadQuery of a test operation
-     */
-    const data = {
-      post: {},
-    };
+    const { post } = usePreloadedQuery<PostLikeButtonSpecQuery>(
+      graphql`
+        query PostLikeButtonSpecQuery($id: ID!) @relay_test_operation {
+          post: node(id: $id) {
+            ...PostLikeButton_post
+          }
+        }
+      `,
+      preloadedQuery,
+    );
 
-    return <PostLikeButton post={data.post} />;
+    return <PostLikeButton post={post} />;
   };
 
   return withProviders({
@@ -36,37 +41,27 @@ it('should render post like button and likes count', async () => {
   const PostLikeButtonSpecQuery = require('./__generated__/PostLikeButtonSpecQuery.graphql');
 
   const postId = 'postId';
-  // eslint-disable-next-line
   const query = PostLikeButtonSpecQuery;
-  // eslint-disable-next-line
   const variables = {
     id: postId,
   };
 
-  /**
-   * TODO
-   * properly mock resolvers
-   */
-  // eslint-disable-next-line
   const customMockResolvers = {
-    Post: () => ({}),
+    Post: () => ({
+      id: postId,
+      cotent: 'Testing count',
+      likesCount: 10,
+      meHasLiked: true,
+    }),
   };
 
-  /**
-   * TODO
-   * queue a pending operation, this would be a preloadQuery call
-   */
+  Environment.mock.queuePendingOperation(query, variables);
 
-  /**
-   * TODO
-   * mock a queued operation
-   */
+  Environment.mock.queueOperationResolver(operation => MockPayloadGenerator.generate(operation, customMockResolvers));
 
-  /**
-   * TODO
-   * preloadQuery test GraphQL operation
-   */
-  const preloadedQuery = {};
+  const preloadedQuery = preloadQuery(Environment, PostLikeButtonSpecQuery, variables, {
+    fetchPolicy: 'store-or-network',
+  });
 
   const Root = getRoot({
     Component: PostLikeButton,
@@ -74,10 +69,48 @@ it('should render post like button and likes count', async () => {
   });
 
   // eslint-disable-next-line
-  const { debug, getByText, getByTestId } = render(<Root />);
+  const { debug, getByText, getByTestId } = render(<Root preloadedQuery={preloadQuery} />);
+
+  expect(getByText('10')).toBeTruthy();
+  expect(getByTestId('likeCount')).toBeInTheDocument();
+});
+
+it('should not render post like count', async () => {
+  const PostLikeButtonSpecQuery = require('./__generated__/PostLikeButtonSpecQuery.graphql');
+
+  const postId = 'postId';
+  const query = PostLikeButtonSpecQuery;
+  const variables = {
+    id: postId,
+  };
+
+  const customMockResolvers = {
+    Post: () => ({
+      id: postId,
+      cotent: 'Testing count',
+      likesCount: 0,
+      meHasLiked: true,
+    }),
+  };
+
+  Environment.mock.queuePendingOperation(query, variables);
+
+  Environment.mock.queueOperationResolver(operation => MockPayloadGenerator.generate(operation, customMockResolvers));
+
+  const preloadedQuery = preloadQuery(Environment, PostLikeButtonSpecQuery, variables, {
+    fetchPolicy: 'store-or-network',
+  });
+
+  const Root = getRoot({
+    Component: PostLikeButton,
+    preloadedQuery,
+  });
+
+  // eslint-disable-next-line
+  const { debug, queryByTestId, queryByText } = render(<Root preloadedQuery={preloadQuery} />);
 
   debug();
 
-  // it should render likes count
-  expect(getByText('10')).toBeTruthy();
+  expect(queryByText('0')).not.toBeInTheDocument();
+  expect(queryByTestId('likeCount')).not.toBeInTheDocument();
 });
